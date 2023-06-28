ff064705d01ef66b6725afc816550fb4a515a3f2
Ojhara <128177684+Ojhara@users.noreply.github.com>
2023-06-27 22:35:09 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('mouseover', function(e) {
+    const x = e.pageX - btn.offsetLeft;
+    const y = e.pageY - btn.offsetTop;
+
+    const btnFill = document.querySelector('.btn-fill');
+    btnFill.style.top = y + 'px';
+    btnFill.style.left = x + 'px';
+});
